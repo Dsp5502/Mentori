52001e4230baa348f864468400dfcc68edbfd325
David Puerto Guerrero <dsp5502@gmail.com>
2022-04-30 22:45:03 -0500

    responsive home y navbar

--- src/Components/Home/Main.js
+++ src/Components/Home/Main.js
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 
 const Main = () => {
   return (
-    <section className='flex w-full justify-center items-center  mx-5 px-5'>
-      <div className='w-1/2'>
+    <section className='flex w-full flex-col-reverse lg:flex-row justify-center items-center  mx-5 px-5'>
+      <div className='w-11/12 lg:w-1/2'>
         <h1 className='text-6xl text-white font-bold mr-5  mb-5'>
           Mentori Te acerca a tus Monitorias{' '}
         </h1>
@@ -14,12 +14,12 @@ const Main = () => {
           en las areas que tienes dificultades.
         </p>
         <Link to='/mentori'>
-          <button className='bg-green-600 px-5 py-2 rounded-2xl text-white w-1/2 mx-auto'>
+          <button className='bg-green-600 px-5 py-2 rounded-2xl text-white  mb-5 lg:mb-5  w-full  lg:w-1/2 mx-auto'>
             Acceder a Mentori
           </button>
         </Link>
       </div>
-      <div className='w-1/2 m '>
+      <div className='w-full lg:w-1/2  '>
         <img
           className='w-5/6'
           src='https://res.cloudinary.com/djjgtili7/image/upload/v1651008444/4630062-removebg_osensc.png'
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,29 +1,113 @@
-import React from 'react';
+import { faBars, faClose } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { logoutAsync } from '../Redux/Actions/actionLogin';
 
 const Navbar = ({ logueadoBtn }) => {
+  const [hamburguer, setHamburguer] = useState(false);
   const dispatch = useDispatch();
   const handleLogOut = () => {
     dispatch(logoutAsync());
+    setHamburguer(!hamburguer);
   };
   return (
-    <div className='w-full  border-red-700 bg-black'>
+    <div className='w-full   bg-black'>
+      {hamburguer && (
+        <div className='bg-black bg-opacity-90 w-full  h-full p-4 absolute z-50 left-0 flex flex-col  text-white  ease-in'>
+          <div className='w-full flex justify-end mt-6'>
+            <FontAwesomeIcon
+              className='text-xl text-white hover:text-orange-500   '
+              icon={faClose}
+              onClick={() => setHamburguer(!hamburguer)}
+            />
+          </div>
+
+          <ul className='text-white flex  flex-col gap-10 justify-center items-center mx-5 '>
+            <li>
+              {logueadoBtn !== null ? (
+                <Link
+                  to='/admin'
+                  className='mx-5  text-lg '
+                  onClick={() => setHamburguer(!hamburguer)}
+                >
+                  Administar Mentori
+                </Link>
+              ) : (
+                <Link
+                  to='/'
+                  className='mx-5 text-lg '
+                  onClick={() => setHamburguer(!hamburguer)}
+                >
+                  Inicio
+                </Link>
+              )}
+            </li>
+            <li>
+              <Link
+                to='/ourmonitor'
+                className={logueadoBtn !== null ? 'hidden' : 'mx-5 text-lg  '}
+                onClick={() => setHamburguer(!hamburguer)}
+              >
+                Nuestros Monitores
+              </Link>
+            </li>
+            <li>
+              <Link
+                to='/contacus'
+                className={logueadoBtn !== null ? 'hidden' : 'mx-5 text-lg  '}
+                onClick={() => setHamburguer(!hamburguer)}
+              >
+                Contactenos
+              </Link>
+            </li>
+            <li>
+              {logueadoBtn !== null ? (
+                <button
+                  className='mx-5 text-lg bg-green-600 px-5 py-2 rounded-2xl '
+                  onClick={handleLogOut}
+                >
+                  Salir
+                </button>
+              ) : (
+                <Link
+                  to='/login'
+                  className='mx-5 text-lg bg-green-600 px-5 py-2 rounded-2xl  '
+                  onClick={() => setHamburguer(!hamburguer)}
+                >
+                  Acceder
+                </Link>
+              )}
+            </li>
+          </ul>
+        </div>
+      )}
       <div className='flex justify-between items-center '>
         <img
           className='h-32 w-32 mx-5 my-2'
           src='https://res.cloudinary.com/djjgtili7/image/upload/v1650996417/Mentori-removebg-preview_mldiit.png'
           alt=''
         />
+
         <ul className='text-white flex justify-center items-center mx-5'>
+          <div className=' h-12 mr-4 flex items-center  self-start md:hidden'>
+            <FontAwesomeIcon
+              className='  text-white text-xl hover:text-orange-peel-500  '
+              icon={faBars}
+              onClick={() => setHamburguer(!hamburguer)}
+            />
+          </div>
           <li>
             {logueadoBtn !== null ? (
-              <Link to='/admin' className='mx-5 text-lg'>
+              <Link
+                to='/admin'
+                className='mx-5  text-lg hidden md:inline-block'
+              >
                 Administar Mentori
               </Link>
             ) : (
-              <Link to='/' className='mx-5 text-lg'>
+              <Link to='/' className='mx-5 text-lg hidden md:inline-block'>
                 Inicio
               </Link>
             )}
@@ -31,7 +115,11 @@ const Navbar = ({ logueadoBtn }) => {
           <li>
             <Link
               to='/ourmonitor'
-              className={logueadoBtn !== null ? 'hidden' : 'mx-5 text-lg '}
+              className={
+                logueadoBtn !== null
+                  ? 'hidden'
+                  : 'mx-5 text-lg hidden md:inline-block '
+              }
             >
               Nuestros Monitores
             </Link>
@@ -39,7 +127,11 @@ const Navbar = ({ logueadoBtn }) => {
           <li>
             <Link
               to='/contacus'
-              className={logueadoBtn !== null ? 'hidden' : 'mx-5 text-lg '}
+              className={
+                logueadoBtn !== null
+                  ? 'hidden'
+                  : 'mx-5 text-lg hidden md:inline-block '
+              }
             >
               Contactenos
             </Link>
@@ -47,7 +139,7 @@ const Navbar = ({ logueadoBtn }) => {
           <li>
             {logueadoBtn !== null ? (
               <button
-                className='mx-5 text-lg bg-green-600 px-5 py-2 rounded-2xl'
+                className='mx-5 text-lg bg-green-600 px-5 py-2 rounded-2xl hidden md:inline-block'
                 onClick={handleLogOut}
               >
                 Salir
@@ -55,7 +147,7 @@ const Navbar = ({ logueadoBtn }) => {
             ) : (
               <Link
                 to='/login'
-                className='mx-5 text-lg bg-green-600 px-5 py-2 rounded-2xl '
+                className='mx-5 text-lg bg-green-600 px-5 py-2 rounded-2xl  hidden md:inline-block'
               >
                 Acceder
               </Link>
